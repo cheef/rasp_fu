ea7beaf7036e816dc0eec33b4217955a89c7b632
Ivan Garmatenko <cheef.che@gmail.com>
2009-04-30 15:51:49 +0400

    improvement file object functionality

--- resources/file.php
+++ resources/file.php
@@ -4,33 +4,41 @@
 	require_once RASP_TOOLS_PATH . 'catcher.php';
 	require_once RASP_PATH . 'exception.php';
 
-	class RaspFileOpenException extends RaspException {	public $message = 'File not exist or permission denied';	}
-	class RaspFileAccessException extends RaspException { public $message = "Can't write to file, permission denied"; }
+	class RaspFileException extends RaspException {};
 
 	class RaspFile extends RaspAbstractResource {
 
 		const DEFAULT_FILE_MOD = 'r';
 		const DEFAULT_READ_BLOCK = 4096;
+		const DEFAULT_PERMISSION = 0755;
+		const EXCEPTION_DELETED_FILE_NOT_EXIST = "File not exists, and can't be deleted";
+		const EXCEPTION_FILE_DELETION = "Can't delete file, no permission";
+		const EXCEPTION_FILE_DESTINATION_MISSED = "Can't copy, destination option missed";
+		const EXCEPTION_FILE_ALREADY_EXIST = "Destination file is already exists";
+		const EXCEPTION_FILE_WRITE_ACCESS = "Can't write to file, permission denied";
+		const EXCEPTION_FILE_OPEN = "File not exist or permission denied";
+		const EXCEPTION_FILE_SOURCE_MISSED = "File source option missed";
+		const EXCEPTION_FILE_CHMOD = "Can't set file permissions";
 
-		public $handler = null, $source = null;
+		public $handler = null, $source = null, $size = null;
 
 		public function __construct($options = array()){
 			$this->initilize($options);
 		}
 
 		public function initilize($options){
-			if(isset($options['source'])){
-				$this->source = $options['source'];
-				try {
+			try {
+				if(RaspArray::is_not_empty($options, 'source')){
+					$this->source = $options['source'];
 					$this->handler = @fopen($this->source, RaspArray::index($options, 'mode', self::DEFAULT_FILE_MOD));
-					if(!$this->handler) throw new RaspFileOpenException; return $this->handler;
-				} catch(RaspFileOpenException $e){ RaspCatcher::add($e); }
-			} else return false;
+					if(!$this->handler) throw new RaspFileException(self::EXCEPTION_FILE_OPEN); return $this->handler;
+				} else throw new RaspFileException(self::EXCEPTION_FILE_SOURCE_MISSED);
+			} catch(RaspFileException $e){ RaspCatcher::add($e); }
 		}
 
 		public function write($data){
-			try { if(!$returning = fwrite($this->handler, $data)) throw new RaspFileAccessException; return $returning;}
-			catch(RaspFileAccessException $e) { RaspCatcher::add($e); }
+			try { if(!$returning = fwrite($this->handler, $data)) throw new RaspFileException(self::EXCEPTION_FILE_WRITE_ACCESS); return $returning;}
+			catch(RaspFileException $e) { RaspCatcher::add($e); }
 		}
 
 		public function close(){
@@ -44,6 +52,88 @@
 
 		public function is_eof(){
 			return feof($this->handler);
+		}
+
+		public function is_exists($file = null){
+			if(!empty($file))
+				#static method behavior#
+			 	return file_exists($file);
+			else
+				#object method behavior#
+				return file_exists($this->source);
+		}
+
+		public function copy($options = array()){
+			try {
+				if(RaspArray::is_empty($options, 'destination')) throw new RaspFileException(self::EXCEPTION_FILE_DESTINATION_MISSED);
+				if(self::is_exists($options['destination']) && !RaspArray::index($options, 'rewrite', false)) throw new RaspFileException(self::EXCEPTION_FILE_ALREADY_EXIST);
+				if(RaspArray::is_not_empty($options, 'target')){
+					#static method behavior#
+					return copy($options['target'], $options['destination']);
+				} else {
+					#object method behavior#
+					$new_file = self::create(array('source' => $options['destination'], 'mode' => 'w'));
+					$returning = $new_file->write($this->read_entire());
+					$new_file->close();
+					return $returning;
+				}
+			} catch(RaspFileException $e) { RaspCatcher::add($e); }
+		}
+
+		public function move($options = array()){
+			try {
+				if(RaspArray::is_empty($options, 'destination')) throw new RaspFileException(self::EXCEPTION_FILE_DESTINATION_MISSED);
+				if(self::is_exists($options['destination'])) throw new RaspFileException(self::EXCEPTION_FILE_ALREADY_EXIST);
+				if(RaspArray::is_not_empty($options, 'target'))
+					#static method behavior#
+					return self::copy($options) && self::delete(array('source' => $options['target']));
+				else
+					#object method behavior#
+					return $this->copy($options) && $this->delete();
+			} catch(RaspFileException $e) { RaspCatcher::add($e); }
+		}
+
+		public function size($options = array()){
+			try {
+				if(RaspArray::is_not_empty($options, 'source')) {
+					#static method behavior#
+					if(!self::is_exists($options['source'])) throw new RaspFileException(self::EXCEPTION_DELETED_FILE_NOT_EXIST);
+				 	return filesize($options['source']);
+				} else
+					#object method behavior#
+					return ((RaspArray::index($options, 'forcing', false) || empty($this->size)) ? $this->size = filesize($this->source) : $this->size);
+			} catch(RaspFileException $e) { RaspCatcher::add($e); }
+		}
+
+		public function delete($options = array()){
+			try {
+				if(RaspArray::is_not_empty($options, 'source')){
+					#static method behavior#
+					if(!self::is_exists($options['source'])) throw new RaspFileException(self::EXCEPTION_DELETED_FILE_NOT_EXIST);
+					if(!(@unlink($options['source']))) throw new RaspFileException(self::EXCEPTION_FILE_DELETION);
+					else return true;
+				} else {
+					#object method behavior#
+					fclose($this->handler);
+					if(!$this->is_exists()) throw new RaspFileException(self::EXCEPTION_DELETED_FILE_NOT_EXIST);
+					if(!(@unlink($this->source))) throw new RaspFileException(self::EXCEPTION_FILE_DELETION);
+					$this->__destruct();
+					return true;
+				}
+			} catch(RaspFileException $e) { RaspCatcher::add($e); }
+		}
+
+		public function chmod($mode = self::DEFAULT_PERMISSION){
+			try {
+				if(!chmod($this->source, $mode)) throw new RaspFileException(self::EXCEPTION_FILE_CHMOD);
+				else return true;
+			} catch(RaspFileException $e) { RaspCatcher::add($e); }
+		}
+
+		public function read_entire(){
+			$data = '';
+			while(!$this->is_eof()) $data .= $this->read();
+			return $data;
 		}
 
 		public static function create($options){
